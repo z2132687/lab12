34641499228edc806867e946d10f00bab69148bb
z2132687 <z2132687@gmail.com>
2023-12-12 21:30:41 +0800

    first commit

---
+++ app/src/main/java/com/example/lab10/SecActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sec)
+    }
+}
